cd4aedab97bb8811fd3e1b0006b8af56164ef117
rishi <rek010@eng.ucsd.edu>
2020-07-26 08:49:20 -0700

    keymap v8 update

--- keyboards/rekumar/split/keymaps/windows_v8/keymap.c
+++ keyboards/rekumar/split/keymaps/windows_v8/keymap.c
@@ -5,8 +5,6 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
-#define _COLEMAK 1
-#define _DVORAK 2
 #define _LOWER 3
 #define _RAISE 4
 #define _SOLIDWORKS 5
@@ -17,14 +15,11 @@
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  COLEMAK,
-  DVORAK,
   SOLIDWORKS,
   RAISEBSPC,
   RAISEDEL,
-  SFTSPC,
   SFTENT,
-  CTL_EQL
+  LOWERTAB
 };
 
 #define LOWER  MO(_LOWER)
@@ -33,15 +28,7 @@ enum custom_keycodes {
 #define MOUSE MO(_MOUSE)
 #define SWNUM  MO(_SOLIDWORKS_NUM)
 #define SFT_SPC SFT_T(KC_SPC)
-#define GUI_A  GUI_T(KC_A)
-#define ALT_S  ALT_T(KC_S)
-#define CTL_D  CTL_T(KC_D)
-#define SFT_F  SFT_T(KC_F)
-#define GUI_DOT  GUI_T(KC_DOT)
-#define ALT_L  ALT_T(KC_L)
-#define CTL_K  CTL_T(KC_K)
-#define SFT_J  SFT_T(KC_J)
-#define RAISE_BSPC LT(_RAISE, KC_BSPC)
+
 
 //keystroke logging code 
 unsigned char current_word[50];
@@ -85,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
     KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,               KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   XXXXXXX, \
     KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   XXXXXXX, \
-    KC_LCTL,  GUI_A,  ALT_S,  CTL_D,  KC_F,  KC_G,                KC_H,   KC_J,  CTL_K,  ALT_L,  GUI_DOT, KC_BSPC, \
+    KC_LCTL,  KC_A,   KC_S,   KC_D,   KC_F,  KC_G,                KC_H,   KC_J,   KC_K,   KC_L,   KC_DOT, KC_BSPC, \
     KC_LALT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_SCLN, KC_SLSH, MOUSE, \
-                      KC_TAB,SFT_SPC, TT(_LOWER), KC_ESC,             KC_LEAD, RAISEBSPC, SFTENT   \
+                      KC_LGUI,SFT_SPC,LOWERTAB, KC_ESC,          KC_LEAD, RAISEBSPC, SFTENT   \
 ),
 
 
@@ -97,35 +84,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB,KC_QUES,KC_DQUO,KC_LPRN,KC_RPRN,KC_COLN,        KC_ASTR, KC_7,    KC_8,    KC_9,   KC_CIRC,  XXXXXXX, \
    KC_LCTL,KC_COLN,KC_QUOT,KC_LBRC,KC_RBRC,KC_UNDS,       KC_PLUS, KC_4,    KC_5,    KC_6,   KC_DOT,  KC_DEL, \
    KC_LALT,KC_AT,X(DEGREE),KC_LCBR,KC_RCBR,KC_HASH,         KC_MINS, KC_1,    KC_2,    KC_3,   KC_SLSH,   KC_ENT, \
-                      KC_TAB,SFT_SPC, LOWER, KC_ESC,     KC_ESC, RAISEDEL, KC_0   \
+                      KC_LGUI,SFT_SPC, LOWERTAB, KC_ESC,     KC_ESC, RAISEDEL, KC_0   \
 ),
 
 /* Raise */
 [_RAISE] = LAYOUT(
   KC_ESC,  KC_F1  ,KC_F2   ,KC_F3  ,  KC_F4 ,  KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9  ,KC_F10,  KC_F11 , \
   KC_TAB , KC_BSLS, KC_EXLM, KC_TILD, KC_AMPR, KC_GRV,       KC_HOME,KC_PGDN,KC_PGUP,KC_END ,XXXXXXX,KC_F12 ,\
-  KC_LCTL , KC_SLSH, KC_MINS, CTL_EQL, KC_PLUS, KC_ASTR,       KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,XXXXXXX,KC_DEL, \
+  KC_LCTL , KC_SLSH, KC_MINS, KC_EQL, KC_PLUS, KC_ASTR,       KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,XXXXXXX,KC_DEL, \
   KC_LALT , KC_PERC, KC_LABK, KC_PIPE, KC_RABK, KC_CIRC,      C(G(KC_LEFT)),XXXXXXX,XXXXXXX,C(G(KC_RGHT)),C(KC_SLSH), KC_RSFT, \
-                      KC_TAB,SFT_SPC, LOWER, KC_ESC,         KC_ESC, RAISEDEL, SFTENT   \
+                      KC_LGUI,SFT_SPC, LOWERTAB, KC_ESC,         KC_ESC, RAISEDEL, SFTENT   \
 
 ),
 
 
 /* Adjust (Lower + Raise) */
 [_ADJUST] =  LAYOUT(
-  XXXXXXX, RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  XXXXXXX, MAGIC_UNSWAP_CTL_GUI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         QWERTY, KC_F7, KC_F8, KC_F9, KC_F12,SOLIDWORKS, \
-  XXXXXXX, GUI_T(MAGIC_SWAP_CTL_GUI), ALT_T(KC_VOLD), CTL_T(KC_MUTE), KC_VOLU, XXXXXXX,           XXXXXXX, KC_F4, KC_F5, KC_F6, KC_F11, XXXXXXX, \
-  XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,                      XXXXXXX, KC_F1, KC_F2, KC_F3, KC_F10, XXXXXXX, \
-                      KC_TAB,SFT_SPC, LOWER,  KC_ESC,                       KC_ESC,  RAISE,  SFTENT   \
+  KC_ESC , RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  KC_TAB , MAGIC_UNSWAP_CTL_GUI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         QWERTY, KC_F7, KC_F8, KC_F9, KC_F12,SOLIDWORKS, \
+  KC_LCTL, MAGIC_SWAP_CTL_GUI, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,           XXXXXXX, KC_F4, KC_F5, KC_F6, KC_F11, XXXXXXX, \
+  KC_LALT, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,                      XXXXXXX, KC_F1, KC_F2, KC_F3, KC_F10, XXXXXXX, \
+                      KC_LGUI,SFT_SPC, LOWERTAB,  KC_ESC,                       KC_ESC,  RAISE,  SFTENT   \
 ),
 
 [_MOUSE] = LAYOUT(
      XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, \
      XXXXXXX,XXXXXXX,KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, \
-     XXXXXXX,XXXXXXX,KC_MS_L,KC_MS_D,KC_MS_R,KC_WH_D,        XXXXXXX, KC_BTN1, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, \
-     XXXXXXX,XXXXXXX,KC_WH_L,KC_BTN3,KC_WH_R,XXXXXXX,        C(G(KC_LEFT)), XXXXXXX, XXXXXXX, C(G(KC_RGHT)), XXXXXXX,  XXXXXXX, \
-                     KC_TAB,SFT_SPC, TT(_LOWER), KC_ESC,             KC_ESC, RAISE_BSPC, SFTENT   \
+     KC_LCTL,XXXXXXX,KC_MS_L,KC_MS_D,KC_MS_R,KC_WH_D,        XXXXXXX, KC_BTN1, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, \
+     KC_LALT,XXXXXXX,KC_WH_L,KC_BTN3,KC_WH_R,XXXXXXX,        C(G(KC_LEFT)), XXXXXXX, XXXXXXX, C(G(KC_RGHT)), XXXXXXX,  XXXXXXX, \
+                     KC_LGUI,SFT_SPC, LOWERTAB, KC_ESC,             KC_ESC, RAISE_BSPC, SFTENT   \
 ),
 
 /* SOLIDWORKS */
@@ -152,14 +139,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case SFTSPC:
-            return TAPPING_TERM_CADET;
-        case SFT_F:
-            return TAPPING_TERM_CADET;
-        case SFT_J:
-            return TAPPING_TERM_CADET;
-        case CTL_EQL:
-          return 300;
         default:
             return TAPPING_TERM;
     }
@@ -180,16 +159,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case QWERTY:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_QWERTY);
-      }
-      return false;
-    case COLEMAK:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
-      }
-      return false;
-    case DVORAK:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_DVORAK);
       }
       return false;
     case SOLIDWORKS:
@@ -221,18 +190,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
       }
       return false;
-    case SFTSPC:
-      if (record->event.pressed) {
-        hash_timer = timer_read();
-        register_code(KC_LSFT);
-        last_pressed = 7;
-      } else {
-        unregister_code(KC_LSFT);
-        if ((timer_elapsed(hash_timer) < TAPPING_TERM_CADET) && (last_pressed == 7)){
-          tap_code(KC_SPC);
-        }
-      }
-      return false;
     case SFTENT:
       if (record->event.pressed) {
         hash_timer = timer_read();
@@ -245,15 +202,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
       }
       return false;
-    case CTL_EQL:
+    case LOWERTAB:
       if (record->event.pressed) {
         hash_timer = timer_read();
-        register_code(KC_LCTL);
-        last_pressed = 10;
+        layer_on(_LOWER);
+        last_pressed = 7;
       } else {
-        unregister_code(KC_LCTL);
+        layer_off(_LOWER);
         if ((timer_elapsed(hash_timer) < TAPPING_TERM_CADET) && (last_pressed == 10)){
-          tap_code(KC_EQL);
+          tap_code(KC_TAB);
         }
       }
       return false;
